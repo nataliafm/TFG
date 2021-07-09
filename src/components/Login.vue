f87eaf9a52eda780f5334d9d6a92e20314f835ba
nataliafm <ntcfm1998@gmail.com>
2021-07-09 02:35:01 +0200

    error arreglado de contraste de colores en Login.vue y Registro.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,62 +1,72 @@
 <template>
   <div class="Login">
-    <div class="cajaRegistro">
-      <div class="formRegistro">
-        <b-form @submit.prevent="entrar" v-if="show">
-          <b-form-group
-            id="email"
-            label="Correo electrónico:"
-            label-for="input-email"
-            description=""
-          >
-            <b-form-input
-              id="input-email"
-              v-model="form.email"
-              type="correo"
-              placeholder="Introduce tu e-mail"
-              required
-              :v-model="$v.form.email.$model"
-              :state="validateState('email')"
-              aria-describedby="error-email"
-            ></b-form-input>
-            <b-form-invalid-feedback id="error-email">
-              Debes introducir un correo válido
-            </b-form-invalid-feedback>
-          </b-form-group>
+    <b-container>
+      <b-row>
+        <b-col md="3"></b-col>
+        <b-col md="6">
+          <b-form @submit.prevent="entrar" v-if="show">
+            <b-form-group
+              id="email"
+              label="Correo electrónico:"
+              label-for="input-email"
+              description=""
+              style="color: #4b4453"
+            >
+              <b-form-input
+                id="input-email"
+                v-model="form.email"
+                type="correo"
+                placeholder="Introduce tu e-mail"
+                required
+                :v-model="$v.form.email.$model"
+                :state="validateState('email')"
+                aria-describedby="error-email"
+              ></b-form-input>
+              <b-form-invalid-feedback id="error-email">
+                Debes introducir un correo válido
+              </b-form-invalid-feedback>
+            </b-form-group>
 
-          <b-form-group
-            id="password"
-            label="Contraseña:"
-            label-for="input-contrasenia"
-            description=""
-          >
-            <b-form-input
-              id="input-password"
-              v-model="form.password"
-              type="password"
-              placeholder="Introduce tu contraseña"
-              required
-              :v-model="$v.form.password.$model"
-              :state="validateState('password')"
-              aria-describedby="error-password"
-            ></b-form-input>
-            <b-form-invalid-feedback id="error-password">
-              Debes introducir una contraseña
-            </b-form-invalid-feedback>
-          </b-form-group>
-          <div v-if="emailIncorrecto" style="color: red;"> El e-mail que ha introducido no se corresponde con una cuenta </div>
-          <div v-if="passIncorrecta" style="color: red;"> La contraseña que ha introducido es incorrecta </div>
-          <b-button type="submit" variant="primary">Enviar</b-button>
-        </b-form>
-      </div>
-    </div>
+            <b-form-group
+              id="password"
+              label="Contraseña:"
+              label-for="input-contrasenia"
+              description=""
+              style="color: #4b4453"
+            >
+              <b-form-input
+                id="input-password"
+                v-model="form.password"
+                type="password"
+                placeholder="Introduce tu contraseña"
+                required
+                :v-model="$v.form.password.$model"
+                :state="validateState('password')"
+                aria-describedby="error-password"
+              ></b-form-input>
+              <b-form-invalid-feedback id="error-password">
+                Debes introducir una contraseña
+              </b-form-invalid-feedback>
+            </b-form-group>
+            <div v-if="emailIncorrecto" style="color: red">
+              El e-mail que ha introducido no se corresponde con una cuenta
+            </div>
+            <div v-if="passIncorrecta" style="color: red">
+              La contraseña que ha introducido es incorrecta
+            </div>
+            <b-button type="submit" variant="primary">Enviar</b-button>
+          </b-form>
+        </b-col>
+        <b-col md="3"></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 import firebase from "firebase";
 import { validationMixin } from "vuelidate";
-import { required, email} from 'vuelidate/lib/validators'
+import { required, email } from "vuelidate/lib/validators";
 
 export default {
   mixins: [validationMixin],
@@ -76,18 +86,19 @@ export default {
   validations: {
     form: {
       email: {
-        required, email
+        required,
+        email,
       },
       password: {
-        required
-      }
-    }
+        required,
+      },
+    },
   },
   methods: {
     entrar() {
       var d = this.form;
       var _this = this;
-      
+
       firebase
         .auth()
         .signInWithEmailAndPassword(d.email, d.password)
@@ -102,8 +113,12 @@ export default {
           console.error("Error: ", err);
           var error = err.code;
           switch (error) {
-            case "auth/user-not-found": _this.emailIncorrecto = true; break;
-            case "auth/wrong-password": _this.passIncorrecta = true; break;
+            case "auth/user-not-found":
+              _this.emailIncorrecto = true;
+              break;
+            case "auth/wrong-password":
+              _this.passIncorrecta = true;
+              break;
           }
         });
     },
@@ -130,18 +145,5 @@ li {
 }
 a {
   color: #42b983;
-}
-.cajaRegistro {
-  width: 50%;
-  margin: auto;
-  margin-top: 7%;
-  background-color: #4B4453;
-  color: #B0A8B9;
-  padding-top: 1%;
-  padding-bottom: 1%;
-}
-.formRegistro {
-  width: 80%;
-  margin: auto;
 }
 </style>
--- src/components/Registro.vue
+++ src/components/Registro.vue
@@ -1,104 +1,118 @@
 <template>
   <div class="Registro">
-    <div class="cajaRegistro">
-      <div class="formRegistro">
-        <b-form @submit.prevent="submit" v-if="show">
-          <b-form-group
-            id="email"
-            label="Correo electrónico:"
-            label-for="input-email"
-            description=""
-          >
-            <b-form-input
-              id="input-email"
-              name="email"
-              v-model="form.email"
-              type="email"
-              placeholder="Introduce tu e-mail"
-              required
-              :v-model="$v.form.email.$model"
-              :state="validateState('email')"
-              aria-describedby="error-email"
-            ></b-form-input>
-            <b-form-invalid-feedback id="error-email">
-              Debes introducir un correo válido
-            </b-form-invalid-feedback>
-            <div v-if="emailEnUso" style="color: red;"> Este e-mail ya está asociado a una cuenta.</div>
-          </b-form-group>
+    <b-container>
+      <b-row>
+        <b-col md="3"></b-col>
+        <b-col md="6">
+          <b-form @submit.prevent="submit" v-if="show">
+            <b-form-group
+              id="email"
+              label="Correo electrónico:"
+              label-for="input-email"
+              description=""
+              style="color: #4b4453"
+            >
+              <b-form-input
+                id="input-email"
+                name="email"
+                v-model="form.email"
+                type="email"
+                placeholder="Introduce tu e-mail"
+                required
+                :v-model="$v.form.email.$model"
+                :state="validateState('email')"
+                aria-describedby="error-email"
+              ></b-form-input>
+              <b-form-invalid-feedback id="error-email">
+                Debes introducir un correo válido
+              </b-form-invalid-feedback>
+              <div v-if="emailEnUso" style="color: red">
+                Este e-mail ya está asociado a una cuenta.
+              </div>
+            </b-form-group>
 
-          <b-form-group
-            id="username"
-            label="Nombre de usuario:"
-            label-for="input-username"
-            description="Podrás cambiar el nombre de usuario más adelante"
-          >
-            <b-form-input
-              id="input-username"
-              v-model="form.username"
-              type="text"
-              placeholder="Introduce tu nombre de usuario"
-              required
-              :v-model="$v.form.username.$model"
-              :state="validateState('username')"
-              aria-describedby="error-username"
-            ></b-form-input>
-            <b-form-invalid-feedback id="error-username">
-              Debes introducir un nombre de usuario, y debe tener una longitud mínima de 9 caracteres
-            </b-form-invalid-feedback>
-          </b-form-group>
+            <b-form-group
+              id="username"
+              label="Nombre de usuario:"
+              label-for="input-username"
+              description="Podrás cambiar el nombre de usuario más adelante"
+              style="color: #4b4453"
+            >
+              <b-form-input
+                id="input-username"
+                v-model="form.username"
+                type="text"
+                placeholder="Introduce tu nombre de usuario"
+                required
+                :v-model="$v.form.username.$model"
+                :state="validateState('username')"
+                aria-describedby="error-username"
+              ></b-form-input>
+              <b-form-invalid-feedback id="error-username">
+                Debes introducir un nombre de usuario, y debe tener una longitud
+                mínima de 9 caracteres
+              </b-form-invalid-feedback>
+            </b-form-group>
 
-          <b-form-group
-            id="password"
-            label="Contraseña:"
-            label-for="input-contrasenia"
-            description=""
-          >
-            <b-form-input
-              id="input-password"
-              v-model="form.password"
-              type="password"
-              required
-              :v-model="$v.form.password.$model"
-              :state="validateState('password')"
-              aria-describedby="error-password"
-            ></b-form-input>
-            <b-form-invalid-feedback id="error-password">
-              Debes introducir una contraseña, y debe tener una longitud mínima de 9 caracteres y contener números, mayúsculas y minúsculas
-          </b-form-invalid-feedback>
-          </b-form-group>
+            <b-form-group
+              id="password"
+              label="Contraseña:"
+              label-for="input-contrasenia"
+              description=""
+              style="color: #4b4453"
+            >
+              <b-form-input
+                id="input-password"
+                v-model="form.password"
+                type="password"
+                required
+                :v-model="$v.form.password.$model"
+                :state="validateState('password')"
+                aria-describedby="error-password"
+              ></b-form-input>
+              <b-form-invalid-feedback id="error-password">
+                Debes introducir una contraseña, y debe tener una longitud
+                mínima de 9 caracteres y contener números, mayúsculas y
+                minúsculas
+              </b-form-invalid-feedback>
+            </b-form-group>
 
-          <b-form-group
-            id="passIgual"
-            label="Vuelve a introducir la contraseña:"
-            label-for="input-contrasenia-igual"
-            description=""
-          >
-            <b-form-input
-              id="input-contrasenia-igual"
-              v-model="form.passIgual"
-              type="password"
-              required
-              :v-model="$v.form.passIgual.$model"
-              :state="validateState('passIgual')"
-              aria-describedby="error-passIgual"
-            ></b-form-input>
-            <b-form-invalid-feedback id="error-passIgual">
-              Introduce la misma contraseña
-          </b-form-invalid-feedback>
-          </b-form-group>
-          <b-button type="submit" variant="primary">Enviar</b-button>
-        </b-form>
-      </div>
-    </div>
+            <b-form-group
+              id="passIgual"
+              label="Vuelve a introducir la contraseña:"
+              label-for="input-contrasenia-igual"
+              description=""
+              style="color: #4b4453"
+            >
+              <b-form-input
+                id="input-contrasenia-igual"
+                v-model="form.passIgual"
+                type="password"
+                required
+                :v-model="$v.form.passIgual.$model"
+                :state="validateState('passIgual')"
+                aria-describedby="error-passIgual"
+              ></b-form-input>
+              <b-form-invalid-feedback id="error-passIgual">
+                Introduce la misma contraseña
+              </b-form-invalid-feedback>
+            </b-form-group>
+            <b-button type="submit" variant="primary">Enviar</b-button>
+          </b-form>
+        </b-col>
+        <b-col md="3"></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 import firebase from "firebase";
 import { validationMixin } from "vuelidate";
-import { required, email, minLength} from 'vuelidate/lib/validators'
-const contieneNumeros = (value) => /\d/.test(value)
-const contieneMayusculas  = (value) => /[a-z]/.test(value) && /[A-Z]/.test(value)
+import { required, email, minLength } from "vuelidate/lib/validators";
+const contieneNumeros = (value) => /\d/.test(value);
+const contieneMayusculas = (value) =>
+  /[a-z]/.test(value) && /[A-Z]/.test(value);
 
 export default {
   mixins: [validationMixin],
@@ -110,7 +124,7 @@ export default {
         email: "",
         username: "",
         password: "",
-        passIgual: ""
+        passIgual: "",
       },
       error: null,
       show: true,
@@ -120,39 +134,45 @@ export default {
   validations: {
     form: {
       email: {
-        required, email
+        required,
+        email,
       },
       username: {
-        required, minLength: minLength(9)
+        required,
+        minLength: minLength(9),
       },
       password: {
-        required, minLength: minLength(9), contieneNumeros, contieneMayusculas
+        required,
+        minLength: minLength(9),
+        contieneNumeros,
+        contieneMayusculas,
       },
       passIgual: {
-        required, esIgual(){
+        required,
+        esIgual() {
           return this.form.password == this.form.passIgual;
-        }
-      }
-    }
+        },
+      },
+    },
   },
   methods: {
-    esIgual(){
+    esIgual() {
       return this.form.password == this.form.passIgual;
     },
     submit() {
       var d = this.form;
       var _this = this;
-      
-      firebase.auth()
+
+      firebase
+        .auth()
         .createUserWithEmailAndPassword(d.email, d.password)
-        .then(function() {
+        .then(function () {
           _this.emailEnUso = false;
 
           var ident = firebase.auth().currentUser.uid;
           var db = firebase.firestore();
 
-          db
-            .collection('Usuario')
+          db.collection("Usuario")
             .doc(ident)
             .set({
               idUsuario: ident,
@@ -170,11 +190,11 @@ export default {
               reviews: [],
               amigos: [],
               notas: {},
-              ultimoCap: []
+              ultimoCap: [],
             })
             .then(function () {
               console.log("Document successfully written!");
-              _this.$router.push({path: '/'});
+              _this.$router.push({ path: "/" });
             })
             .catch(function (error) {
               console.log("Error writing document: ", error);
@@ -183,7 +203,9 @@ export default {
         .catch((err) => {
           this.error = err.code;
           switch (this.error) {
-            case "auth/email-already-in-use": this.emailEnUso = true; break;
+            case "auth/email-already-in-use":
+              this.emailEnUso = true;
+              break;
           }
         });
     },
@@ -210,18 +232,5 @@ li {
 }
 a {
   color: #42b983;
-}
-.cajaRegistro {
-  width: 50%;
-  margin: auto;
-  margin-top: 7%;
-  background-color: #4B4453;
-  color: #B0A8B9;
-  padding-top: 1%;
-  padding-bottom: 1%;
-}
-.formRegistro {
-  width: 80%;
-  margin: auto;
 }
 </style>
